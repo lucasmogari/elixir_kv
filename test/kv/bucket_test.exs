ffb509cbd1a87f03883b9f53a084c924ce133e87
Lucas Mogari <lucasmogari@gmail.com>
2017-03-29 03:59:48 -0300

    Delete bucket

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -12,4 +12,12 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "delete value by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "egg", 1)
+    assert KV.Bucket.get(bucket, "egg") == 1
+
+    assert KV.Bucket.delete(bucket, "egg") == 1
+    assert KV.Bucket.get(bucket, "egg") == nil
+  end
 end
